0c180b3980c5b801b5b283bb287d336d05e7b4c0
cornelber <bercornel@gmail.com>
2025-02-13 17:08:17 +0200

    implemented sidebar initial structure

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,16 +1,10 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { signOut, useSession } from "next-auth/react";
 
 export default function DashboardPage() {
-  const { data: session } = useSession();
 
   return (
     <div>
-      {session ? <h1>Welcome, {session.user.username}!</h1> : <p>Loading...</p>}
-      <Button onClick={() => signOut({ callbackUrl: "/login" })}>
-        Log out
-      </Button>
+      dashboard
     </div>
   );
 }
